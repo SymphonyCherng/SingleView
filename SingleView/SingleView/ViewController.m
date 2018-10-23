187d56e138c7aa50387cacc85725a747a0a45895
Symphony <symphony.cherng@qq.com>
2018-10-23 12:17:47 +0800

    UILabel
    test

--- SingleView/SingleView/ViewController.m
+++ SingleView/SingleView/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(20, 100, 280, 30)];
+    label.text = @"Hello World";
+    [self.view addSubview:label];
 }
 
 
